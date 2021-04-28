0e558b91a9b2e798d1076611c22843bbe58c35bd
DanielDruetta <danidruetta_97@hotmail.com>
2021-04-28 20:31:27 -0300

    Cambios en el factory conexion

--- src/data/FactoryConexion.java
+++ src/data/FactoryConexion.java
@@ -5,17 +5,26 @@ import java.sql.*;
 public class FactoryConexion {
 
 	private static FactoryConexion instancia;
-
+	
+	private String driver = "com.mysql.jdbc.Driver";
+	private String host = "node68248-alquilazo.jelastic.saveincloud.net";
+	private String port = "3306";
+	private String user = "root";
+	private String password = "MGXZHG1Wy0";
+	private String db = "futbol";
+	private int conectados = 0;
+	private Connection conn = null;
+/*
 	private String driver = "com.mysql.jdbc.Driver";
 	private String host = "localhost";
 	private String port = "3306";
 	private String user = "root";
-	private String password = "admin";
-	//private String password = "password";
+	//private String password = "admin";
+	private String password = "password";
 	private String db = "futbol";
 	private int conectados = 0;
 	private Connection conn = null;
-
+*/
 	private FactoryConexion() {
 		try {
 			Class.forName(driver);
